2ecd69ed57bb7635bdd0ac5f20328ba962fdd7cb
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-01-15 21:23:22 +0100

    fixed

--- script.js
+++ script.js
@@ -33,40 +33,6 @@ var first = React.createClass({
 
 var element = React.createElement(first);
 ReactDOM.render(element, document.getElementById('first'));
-
-
-var second = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
-    componentDidUpdate: function() {
-        console.log('componentDidUpdate');
-    },
-
-   
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-
-    decrement: function() {
-        this.setState ({
-            counter: this.state.counter - 1
-        });
-    },
-
-    render: function() {
-        return  React.createElement('div',  {className: 'counter'}, 
-          React.createElement('div', {id:'counter'}, this.state.counter),
-          React.createElement('button', {onClick:this.increment}, 'Dodaj'),
-          React.createElement('button', {onClick:this.decrement}, 'Odejmij')
-         );     
-    }
-});
-
-var element = React.createElement(second);
 ReactDOM.render(element, document.getElementById('second'));
+ReactDOM.render(element, document.getElementById('three'));
+
